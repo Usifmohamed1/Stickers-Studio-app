d7752ef3cf82b8ceb74c0dc10f92c257c2733428
Youssef Mohamed <122700643+Usifmohamed1@users.noreply.github.com>
2025-08-19 14:45:53 +0300

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,16 @@
+import { ShoppingCart } from "lucide-react";
+
 function CategoryCard({ title, onClick, img }) {
   return (
-  <button onClick={onClick} className="group relative overflow-hidden rounded-2xl shadow-2xl aspect-[12.7/16] text-left active:scale-95 transition-transform">
-      <img src={img} alt={title} className="absolute inset-0 w-full h-full object-contain group-hover:scale-105 transition-transform duration-300" />
+    <button
+      onClick={onClick}
+      className="group relative overflow-hidden rounded-2xl shadow-2xl aspect-[12.7/16] text-left active:scale-95 transition-transform"
+    >
+      <img
+        src={img}
+        alt={title}
+        className="absolute inset-0 w-full h-full object-contain group-hover:scale-105 transition-transform duration-300"
+      />
       <div className="absolute inset-0 bg-gradient-to-t from-black/30 to-black/10" />
       <div className="absolute bottom-3 left-3 text-white">
         <div className="text-xl font-bold">{title}</div>
@@ -9,7 +18,6 @@ function CategoryCard({ title, onClick, img }) {
     </button>
   );
 }
-import { ShoppingCart } from "lucide-react";
 
 function Home({ gotoCategory, onGoCart, totalQty }) {
   return (
@@ -19,11 +27,10 @@ function Home({ gotoCategory, onGoCart, totalQty }) {
           Choose a Category
         </p>
 
-        {/* Show the cart icon button only if there are items in the cart (mobile view) */}
         {totalQty > 0 && (
           <button
             onClick={onGoCart}
-            className={`relative p-2 rounded-full block md:hidden active:scale-95 focus:scale-95 text-sky-500 hover:scale-110 transition-transform duration-200`}
+            className="relative p-2 rounded-full block md:hidden active:scale-95 focus:scale-95 text-sky-500 hover:scale-110 transition-transform duration-200"
             aria-label="Go to Cart"
           >
             <ShoppingCart size={28} />
@@ -33,8 +40,14 @@ function Home({ gotoCategory, onGoCart, totalQty }) {
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-14 mb-10 mt-5 active:scale-95 transition-transform">
-        <CategoryCard onClick={() => gotoCategory("stickers")} img="https://i.postimg.cc/bNW9QV3n/stickers.png" />
-        <CategoryCard onClick={() => gotoCategory("posters")} img="https://i.postimg.cc/nVYLKX2G/pos-358-x-452-px.png" />
+        <CategoryCard
+          onClick={() => gotoCategory("stickers")}
+          img="https://i.postimg.cc/bNW9QV3n/stickers.png"
+        />
+        <CategoryCard
+          onClick={() => gotoCategory("posters")}
+          img="https://i.postimg.cc/nVYLKX2G/pos-358-x-452-px.png"
+        />
       </div>
     </section>
   );
